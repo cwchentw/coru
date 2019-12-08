6afb4a49bed03683a7f28ed6ad6a681f360e13e8
Michael Chen <cwchen.tw@outlook.com>
2019-12-08 07:01:24 +0000

    Update tests

---
+++ scripts/hello.m
@@ -0,0 +1,19 @@
+#import <Foundation/Foundation.h>
+#include <stdio.h>
+
+int main(void)
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    if (!pool) {
+        fprintf(stderr, "Failed to allocate auto release pool\n");
+        fprintf(stderr, "Check available system memory\n");
+        return 1;
+    }
+
+    NSString *greet = [[[NSString alloc] initWithString: @"Hello World"] autorelease];
+    printf("%s\n", [greet UTF8String]);
+
+    [pool drain];
+
+    return 0;
+}
