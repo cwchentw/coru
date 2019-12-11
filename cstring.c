a8d08c9eca6911959757222d1ae171e753f397d2
Michael Chen <cwchen123@gmail.com>
2019-12-11 23:35:29 +0800

    Refactor some code

--- cstring.c
+++ cstring.c
@@ -28,14 +28,8 @@ BOOL string_is_equal(char *a, char *b)
 
 BOOL string_starts_with(char *a, char *b)
 {
-    size_t sz_a = strlen(a);
-    size_t sz_b = strlen(b);
-
-    if (sz_a < sz_b)
-        return FALSE;
-
     size_t i = 0;
-    while (i < sz_b) {
+    while (a[i] && b[i]) {
         if (a[i] != b[i])
             return FALSE;
 
@@ -47,19 +41,13 @@ BOOL string_starts_with(char *a, char *b)
 
 BOOL string_contains(char *a, char *b)
 {
-    size_t sz_a = strlen(a);
-    size_t sz_b = strlen(b);
-
-    if (sz_a < sz_b)
-        return FALSE;
-
     size_t i = 0;
-    while (i + sz_b <= sz_a) {
+    while (a[i]) {
         size_t _i = i;
         size_t _j = 0;
 
         BOOL matched = TRUE;
-        while (_j < sz_b) {
+        while (b[_j]) {
             if (a[_i] != b[_j]) {
                 matched = FALSE;
                 break;
