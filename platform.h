311b3a2708fc37bb548a029027afec14a712618f
Michael Chen <cwchen.tw@outlook.com>
2019-12-14 11:40:15 +0000

    Refactor some code

--- platform.h
+++ platform.h
@@ -1,6 +1,18 @@
+/** @file       platform.h
+ *  @brief      Platform-specific data
+ *  @author     Michael Chen
+ *  @copyright  MIT
+ *
+ *  The macro definitions seen in this document represent the platform data of Unix.
+ */
 #ifndef CLIBS_PLATFORM_H
 #define CLIBS_PLATFORM_H
 
+/** @def END_OF_LINE
+ *  @brief End of line of specific host.
+ *
+ *  Currently, EOL_OF_LINE works on Windows and Unix.
+ */
 #ifndef END_OF_LINE
     #ifdef _WIN32
         #define END_OF_LINE "\r\n"
@@ -11,6 +23,11 @@
     #endif
 #endif  /* END_OF_LINE */
 
+/** @def DIRECTORY_SEPARATOR
+ *  @brief Directory separator of specific host.
+ *
+ *  Currently, DIRECTORY_SEPARATOR works on Windows and Unix.
+ */
 #ifndef DIRECTORY_SEPARATOR
     #ifdef _WIN32
         #define DIRECTORY_SEPARATOR "\\"
@@ -19,6 +36,6 @@
     #else
         #error "Unsupported platform"
     #endif
-#endif
+#endif  /* DIRECTORY_SEPARATOR */
 
 #endif  /* CLIBS_PLATFORM_H */
