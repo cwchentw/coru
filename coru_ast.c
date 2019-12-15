121b965474b1049e9f127c60f6fc9a797fcd4b43
Michael Chen <cwchen123@gmail.com>
2019-12-15 15:38:25 +0800

    Update coru ast

--- coru_ast.c
+++ coru_ast.c
@@ -20,6 +20,7 @@ static BOOL _coru_ast_backslash_add(
     coru_ast_backslash_t *self, coru_token_t *token);
 static void _coru_ast_backslash_delete(void *self);
 static coru_ast_string_t * _coru_ast_string_new(void);
+static BOOL _coru_ast_string_add(coru_ast_string_t *self, coru_token_t *token);
 static void _coru_ast_string_delete(void *self);
 
 struct coru_ast_t {
@@ -343,6 +344,53 @@ static coru_ast_string_t * _coru_ast_string_new(void)
     }
 
     return ast;
+}
+
+static BOOL _coru_ast_string_expand(coru_ast_string_t *self);
+
+static BOOL _coru_ast_string_add(coru_ast_string_t *self, coru_token_t *token)
+{
+    if (!_coru_ast_string_expand(self))
+        return FALSE;
+
+    if (0 == self->size) {
+        self->tokens[self->size] = token;
+        self->size += 1;
+    }
+    else {
+        self->size += 1;
+        self->tokens[self->size] = token;
+    }
+
+    return TRUE;
+}
+
+static BOOL _coru_ast_string_expand(coru_ast_string_t *self)
+{
+    if (self->size < self->capacity)
+        return TRUE;
+
+    self->capacity <<= 1;
+    coru_token_t **old_tokens = self->tokens;
+    coru_token_t **new_tokens = \
+        (coru_token_t **) \
+        malloc(self->capacity * sizeof(coru_token_t *));
+    if (!new_tokens) {
+        PUTERR("Failed to allocate memory for tokens in coru ast");
+        PUTERR("Check available system memory");
+        return FALSE;
+    }
+
+    {
+        size_t i;
+        for (i = 0; i < self->size; i++)
+            new_tokens[i] = old_tokens[i];
+    }
+
+    self->tokens = new_tokens;
+    free(old_tokens);
+
+    return TRUE;
 }
 
 static void _coru_ast_string_delete(void *self)
