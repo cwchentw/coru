1551d5bc23d2d8ce0305771d7f863be4bf70a416
Michael Chen <cwchen123@gmail.com>
2019-12-21 09:38:29 +0800

    Update coru_parser.c

--- coru_parser.c
+++ coru_parser.c
@@ -100,6 +100,13 @@ BOOL coru_parser_parse(coru_parser_t *self, coru_lexer_t *lexer)
             token = coru_lexer_next(lexer);  /* Pass. */
         }
 
+        /* Check and fix it later. */
+        /*
+        if (ast) {
+            self->asts[self->size] = ast;
+            self->size += 1;
+        }
+        */
         if (ast) {
             if (0 == self->size) {
                 self->asts[self->size] = ast;
