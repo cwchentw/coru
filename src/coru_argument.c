74e342a45787f922984d5443f3b7e71eea8b9012
Michelle Chen <cwchen.tw@outlook.com>
2021-07-17 06:31:37 +0000

    Treat source as Ruby; update some code

--- src/coru_argument.c
+++ src/coru_argument.c
@@ -17,23 +17,26 @@ struct coru_argument_t {
 };
 
 #define _is_c(opt) \
-    (0 == strcmp(opt, "-c") || 0 == strcmp(opt, "--as-c"))
+    (0 == strcmp((opt), "-c") || 0 == strcmp((opt), "--as-c"))
 #define _is_cpp(opt) \
-    (0 == strcmp(opt, "-cpp") || 0 == strcmp(opt, "-cxx") \
-        || 0 == strcmp(opt, "-c++") || 0 == strcmp(opt, "--as-cpp") \
-        || 0 == strcmp(opt, "--as-cxx") || 0 == strcmp(opt, "--as-c++"))
+    (0 == strcmp((opt), "-cpp") || 0 == strcmp((opt), "-cxx") \
+        || 0 == strcmp((opt), "-c++") || 0 == strcmp((opt), "--as-cpp") \
+        || 0 == strcmp((opt), "--as-cxx") || 0 == strcmp((opt), "--as-c++"))
 #define _is_java(opt) \
-    (0 == strcmp(opt, "-java") || 0 == strcmp(opt, "--as-java"))
+    (0 == strcmp((opt), "-java") || 0 == strcmp((opt), "--as-java"))
 #define _is_csharp(opt) \
-    (0 == strcmp(opt, "-c#") || 0 == strcmp(opt, "-csharp") \
-        || 0 == strcmp(opt, "--as-c#") || 0 == strcmp(opt, "--as-csharp"))
+    (0 == strcmp((opt), "-c#") || 0 == strcmp((opt), "-csharp") \
+        || 0 == strcmp((opt), "--as-c#") || 0 == strcmp((opt), "--as-csharp"))
 #define _is_python(opt) \
-    (0 == strcmp(opt, "-py") || 0 == strcmp(opt, "-python") \
-        || 0 == strcmp(opt, "--as-python"))
+    (0 == strcmp((opt), "-py") || 0 == strcmp((opt), "-python") \
+        || 0 == strcmp((opt), "--as-python"))
+#define _is_ruby(opt) \
+    (0 == strcmp((opt), "-rb") || 0 == strcmp((opt), "-ruby") \
+        || 0 == strcmp((opt), "--as-ruby"))
 #define _is_cmake(opt) \
-    (0 == strcmp(opt, "-cmake") || 0 == strcmp(opt, "--as-cmake"))
+    (0 == strcmp((opt), "-cmake") || 0 == strcmp((opt), "--as-cmake"))
 #define _is_make(opt) \
-    (0 == strcmp(opt, "-make") || 0 == strcmp(opt, "--as-make"))
+    (0 == strcmp((opt), "-make") || 0 == strcmp((opt), "--as-make"))
 
 coru_argument_t * coru_argument_parse(int argc, char **argv)
 {
@@ -99,6 +102,10 @@ coru_argument_t * coru_argument_parse(int argc, char **argv)
         }
         else if (_is_python(opt)) {
             arg->lang = LANGUAGE_PYTHON;
+            arg->index += 1;
+        }
+        else if (_is_ruby(opt)) {
+            arg->lang = LANGUAGE_RUBY;
             arg->index += 1;
         }
         else if (_is_cmake(opt)) {
--- src/coru_help.c
+++ src/coru_help.c
@@ -32,6 +32,8 @@ void coru_help_help(FILE *stream)
     fprintf(stream, "    --as-csharp%s", END_OF_LINE);
     fprintf(stream, "    -py -python\t\tTreat target source as Python%s", END_OF_LINE);
     fprintf(stream, "    --as-python%s", END_OF_LINE);
+    fprintf(stream, "    -rb -ruby\t\tTreat target source as Ruby%s", END_OF_LINE);
+    fprintf(stream, "    --as-ruby%s", END_OF_LINE);
     fprintf(stream, "    -cmake\t\tTreat target source as CMakeLists.txt%s", END_OF_LINE);
     fprintf(stream, "    --as-cmake%s", END_OF_LINE);
     fprintf(stream, "    -make\t\tTreat target source as Makefile%s", END_OF_LINE);
