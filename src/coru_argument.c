f2308ea00a6a5de6812d9193b7b94f52dc5015bd
Michelle Chen <cwchen.tw@outlook.com>
2021-07-17 06:54:54 +0000

    Treat source as C shell

--- src/coru_argument.c
+++ src/coru_argument.c
@@ -42,6 +42,8 @@ struct coru_argument_t {
     (0 == strcmp((opt), "-js") || 0 == strcmp((opt), "--as-js"))
 #define _is_objc(opt) \
     (0 == strcmp((opt), "-objc") || 0 == strcmp((opt), "--as-objc"))
+#define _is_csh(opt) \
+    (0 == strcmp((opt), "-csh") || 0 == strcmp((opt), "--as-csh"))
 #define _is_cmake(opt) \
     (0 == strcmp((opt), "-cmake") || 0 == strcmp((opt), "--as-cmake"))
 #define _is_make(opt) \
@@ -131,6 +133,10 @@ coru_argument_t * coru_argument_parse(int argc, char **argv)
         }
         else if (_is_objc(opt)) {
             arg->lang = LANGUAGE_OBJC;
+            arg->index += 1;
+        }
+        else if (_is_csh(opt)) {
+            arg->lang = LANGUAGE_CSH;
             arg->index += 1;
         }
         else if (_is_cmake(opt)) {
--- src/coru_help.c
+++ src/coru_help.c
@@ -42,6 +42,8 @@ void coru_help_help(FILE *stream)
     fprintf(stream, "    --as-js%s", END_OF_LINE);
     fprintf(stream, "    -objc\t\tTreat target source as Objective-C%s", END_OF_LINE);
     fprintf(stream, "    --as-objc%s", END_OF_LINE);
+    fprintf(stream, "    -csh\t\tTreat target source as C Shell%s", END_OF_LINE);
+    fprintf(stream, "    --as-csh%s", END_OF_LINE);
     fprintf(stream, "    -cmake\t\tTreat target source as CMakeLists.txt%s", END_OF_LINE);
     fprintf(stream, "    --as-cmake%s", END_OF_LINE);
     fprintf(stream, "    -make\t\tTreat target source as Makefile%s", END_OF_LINE);
