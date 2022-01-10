6951a5ba3c0188dbed409131b0b4eabacbe66d9b
Michelle Chen <cwchen.tw@outlook.com>
2022-01-10 19:17:49 +0800

    Fix and update some code

--- demo/hello.m
+++ demo/hello.m
@@ -6,13 +6,17 @@ int main(void)
     NSAutoreleasePool *pool = \
         [[NSAutoreleasePool alloc] init];
     if (!pool) {
-        fprintf(stderr, "Failed to allocate auto release pool\n");
-        fprintf(stderr, "Check available system memory\n");
+        fprintf(stderr,
+            "Failed to allocate auto release pool\n");
+        fprintf(stderr,
+            "Check available system memory\n");
         return 1;
     }
 
     NSString *greet = \
-        [[[NSString alloc] initWithString: @"Hello World"] autorelease];
+        [[[NSString alloc]
+            initWithString: @"Hello World"]
+                autorelease];
     printf("%s\n", [greet UTF8String]);
 
     [pool drain];
